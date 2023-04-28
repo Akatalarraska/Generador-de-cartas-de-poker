1053453c9fbf0a005030be7487e9c61aaafb059c
Akatalarraska <125604224+Akatalarraska@users.noreply.github.com>
2023-04-28 17:35:42 +0000

    devol variables arreglada

--- src/app.js
+++ src/app.js
@@ -7,34 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  const randomCard = () => {
-    let naipe = ["♥", "♦", "♣", "♠"];
-    let numbers = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K"
-    ];
 
-    let randomNaipe = naipe[Math.floor(Math.random() * naipe.length)];
-    let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  const naipe = ["♥", "♦", "♣", "♠"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomNaipe = naipe[Math.floor(Math.random() * naipe.length)];
+  let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
 
-    //let miCarta = `${randomNaipe} ${randomNumber} ${randomNaipe}`;
-    //return miCarta;
+  document.querySelector("#top").innerHTML = randomNaipe;
+  document.querySelector("#mid").innerHTML = randomNumber;
+  document.querySelector("#bot").innerHTML = randomNaipe;
+  //let miCarta = `${randomNaipe} ${randomNumber} ${randomNaipe}`;
+  //return miCarta;
 
+  /*
     let cardHTML = document.querySelector("div");
-    cardHTML.innerHTML = randomNumber;
-  };
-  console.log(randomCard());
+    cardHTML.innerHTML = randomNaipe;
+
+    let numberHTML = document.getElementById("mid");
+    numberHTML.innerHTML = randomNumber;
+    */
 };
 
 /*
@@ -47,8 +52,3 @@ paloInferior.innerHTML = palosRandom;
 let valorCarta = document.querySelector(".valor-carta");
 valorCarta.innerHTML = valoresRandom;
 */
-
-
-
-
-};
