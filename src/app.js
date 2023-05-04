fc97aa353c1990337561872c6c105bedc68a224b
Akatalarraska <125604224+Akatalarraska@users.noreply.github.com>
2023-05-04 21:19:09 +0000

    aplicamos el cambio de color dependiendo el naipe

--- src/app.js
+++ src/app.js
@@ -8,7 +8,13 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  const naipe = ["♥", "♦", "♣", "♠"];
+  const naipe = [
+    { symbol: "♥", color: "red" },
+    { symbol: "♦", color: "red" },
+    { symbol: "♣", color: "black" },
+    { symbol: "♠", color: "black" }
+  ];
+
   const numbers = [
     "A",
     "2",
@@ -25,11 +31,21 @@ window.onload = function() {
     "K"
   ];
   let randomNaipe = naipe[Math.floor(Math.random() * naipe.length)];
+  let naipeSymbol = randomNaipe.symbol;
+  let naipeColor = randomNaipe.color;
+
   let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
 
-  document.querySelector("#top").innerHTML = randomNaipe;
+  document.querySelector("#top").innerHTML = naipeSymbol;
+  document.querySelector("#bot").innerHTML = naipeSymbol;
+  if (naipeSymbol === "♥" || naipeSymbol === "♦") {
+    document.querySelector("#top").classList.add("red");
+    document.querySelector("#bot").classList.add("red");
+    document.querySelector("#mid").classList.add("red");
+  }
+
   document.querySelector("#mid").innerHTML = randomNumber;
-  document.querySelector("#bot").innerHTML = randomNaipe;
+
   //let miCarta = `${randomNaipe} ${randomNumber} ${randomNaipe}`;
   //return miCarta;
 
